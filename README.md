ed17aecbe41463685460bd422c36d4d8829e34f1
dawiddominiak <poczta@dawiddominiak.pl>
2020-02-16 14:06:34 +0100

    Project updated, firebase configured

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # TableSoccerFrontend
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.2.5.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 9.0.2.
 
 ## Development server
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgxAuthFirebaseUIModule } from 'ngx-auth-firebaseui';
 
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +12,18 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NgxAuthFirebaseUIModule.forRoot({
+      apiKey: 'AIzaSyBMtiqBynHcurfSncZ5CSolDkNao5508KA',
+      authDomain: 'table-soccer-d5bed.firebaseapp.com',
+      databaseURL: 'https://table-soccer-d5bed.firebaseio.com',
+      projectId: 'table-soccer-d5bed',
+      storageBucket: 'table-soccer-d5bed.appspot.com',
+      messagingSenderId: '273036208473',
+      appId: '1:273036208473:web:35ec8cb1ca387fb7f40448'
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
