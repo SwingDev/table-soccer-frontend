e2d2cd2ed941057ba0522a3ba0d1d1a001273203
dawiddominiak <poczta@dawiddominiak.pl>
2020-02-16 23:48:18 +0100

    Automated login/logout and accessible token

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,6 +1,8 @@
 import { AuthProvider } from 'ngx-auth-firebaseui';
 
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -10,9 +12,25 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
   providers = AuthProvider;
 
-  constructor() { }
+  constructor(
+    private auth: AngularFireAuth,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    this.auth.user.subscribe(
+      async (user) => {
+        if (user) {
+          const token = await user.getIdToken();
+
+          this.router.navigateByUrl('/');
+
+          console.log(user, token);
+        } else {
+          this.router.navigateByUrl('/login');
+        }
+      }
+    );
   }
 
 }
