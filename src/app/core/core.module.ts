e02e9d5d7dc8c68156a8986d3f3b5a562ac6de9d
Wojtek Ryczaj <wojtek.ryczaj@swingdev.io>
2020-02-28 15:37:20 +0100

    Downgrade font-awesome to make it work in production

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { SharedModule } from '../shared/shared.module';
 import { RouterModule } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [HeaderComponent],
   imports: [
     CommonModule,
     SharedModule,
-    RouterModule
+    RouterModule,
+    FontAwesomeModule
   ],
   exports: [
     HeaderComponent
